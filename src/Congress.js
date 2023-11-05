6bffab914798cb879b36be0dd3ab7389cb2238ad
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-11-05 15:09:12 -0500

    created an api call for US congress members

--- src/Congress.js
+++ src/Congress.js
@@ -3,9 +3,49 @@ import React, { useState } from 'react';
 function Congress() 
 {
   const [memberName, setMemberName] = useState('L000174');
-
+  const [fullMemberName, setFullMemberName] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const fetchPartyData = async (name) => {
+    setIsLoading(true);
+    setError(null);
+  
+    try {
+      const response = await fetch(`https://api.congress.gov/v3/member/${encodeURIComponent(name)}?api_key=${process.env.REACT_APP_API_KEY}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      // Check if the response contains items
+      let objectLength = Object.keys(data.member).length;
+
+      if (data.member && objectLength > 0) {
+        // parsing the data about Congress Member
+        const member = data.member;
+        const fullName = member.directOrderName; 
+ 
+        setFullMemberName(fullName);
+      } else {
+        setError('No information not found for this member.');
+      }
+    } catch (e) {
+      if (e.name === "TypeError" && e.message === "Failed to fetch") {
+        setError("Network error. Please check your internet connection.");
+      } else {
+        setError(`Failed to fetch data: ${e.message}`);
+      }
+      console.error(e);
+    }
+  
+    setIsLoading(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchPartyData(memberName);
+  };
 
 
   const handleMemberNameChange = (event) => {
@@ -31,6 +71,11 @@ function Congress()
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
       
+      {fullMemberName && (
+        <div>
+          <p>Full Member Name: {fullMemberName}</p>
+        </div>
+      )}
 
       </div>
     );
