faa41df90b2124c1954dc6150eb27ea8615c7c4e
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-04 20:23:20 -0400

    uploaded pngs, made homepage.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React from 'react';
+import Homepage from './Homepage'; // Import the Homepage component
+
+function App() 
+{
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
+
+export default App;
