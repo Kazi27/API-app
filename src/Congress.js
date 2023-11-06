fe575db9b6db8c3a508573112f4a94bd2052d25a
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-11-06 11:57:47 -0500

    removed code for another PR

--- src/Congress.js
+++ src/Congress.js
@@ -11,9 +11,6 @@ function Congress()
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // New state variables for treaty information
-  const [treatyInput, setTreatyInput] = useState('');
-  const [treatyData, setTreatyData] = useState(null);
 
   const fetchPartyData = async (name) => {
     setIsLoading(true);
@@ -69,51 +66,6 @@ function Congress()
     setMemberName(event.target.value);
   };
 
-  // New function to fetch treaty data
-  const fetchTreatyData = async (input) => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(`https://api.congress.gov/v3/treaty?api_key=${process.env.REACT_APP_API_KEY}&input=${encodeURIComponent(input)}`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-
-      // Check if the response contains items
-      if (data.treaties && data.treaties.length > 0) {
-        // Parsing the data about treaties
-        const treaty = data.treaties[0];
-        const treatySubject = treaty.treatySubject;
-
-        setTreatyData(treatySubject);
-      } else {
-        setError('No information found for this treaty input.');
-      }
-    } catch (e) {
-      if (e.name === "TypeError" && e.message === "Failed to fetch") {
-        setError("Network error. Please check your internet connection.");
-      } else {
-        setError(`Failed to fetch treaty data: ${e.message}`);
-      }
-      console.error(e);
-    }
-
-    setIsLoading(false);
-  };
-
-  // Event handler for submitting the treaty form
-  const handleTreatySubmit = (event) => {
-    event.preventDefault();
-    fetchTreatyData(treatyInput);
-  };
-
-  // Event handler for updating the treaty input field
-  const handleTreatyInputChange = (event) => {
-    setTreatyInput(event.target.value);
-  };
-  
     return (
       <div>
         <h1>Congress Page</h1>
@@ -141,28 +93,9 @@ function Congress()
         </div>
       )}
 
-      <h2>Treaty Information</h2>
-      <form onSubmit={handleTreatySubmit}>
-        <label>
-          Congress Treaty Information:
-          <input
-            type="text"
-            value={treatyInput}
-            onChange={handleTreatyInputChange}
-          />
-        </label>
-        <button type="submit">Search Treaty</button>
-      </form>
-
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
       
-
-      {treatyData && (
-        <div>
-          <p>Treaty Information: {treatyData}</p>
-        </div>
-      )}
 
       </div>
     );
