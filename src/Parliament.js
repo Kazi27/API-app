908ee2b1a7d8efc8d28f0cb1a58e1de83588ab34
Rahatk1 <rahatkhandokar5@gmail.com>
2023-11-08 00:36:25 -0500

    tried implemtning treaty

--- src/Parliament.js
+++ src/Parliament.js
@@ -1,207 +1,156 @@
 import React, { useState } from 'react';
 
 const Parliament = () => {
-// used for handling the name inputted by the user
-const [memberName, setMemberName] = useState('');
-// state to hold the values of the currently searched parliament member's name, gender, party, and membership information
-const [fullMemberName, setFullMemberName] = useState(null);
-const [partyInfo, setPartyInfo] = useState(null);
-const [gender, setGender] = useState(null);
-const [latestMembership, setLatestMembership] = useState(null);
-const [startDate, setStartDate] = useState(null);
-const [membershipEnd, setMembershipEnd] = useState(null);
-const [membershipStatus, setMembershipStatus] = useState(null);
-//used for error handling and used in cases where the inputted name is blank or not a part of the api dataset 
-const [error, setError] = useState(null);
+  // State hooks for party member data
+  const [memberName, setMemberName] = useState('');
+  const [fullMemberName, setFullMemberName] = useState(null);
+  const [partyInfo, setPartyInfo] = useState(null);
+  const [gender, setGender] = useState(null);
+  const [latestMembership, setLatestMembership] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [membershipEnd, setMembershipEnd] = useState(null);
+  const [membershipStatus, setMembershipStatus] = useState(null);
+  const [error, setError] = useState(null);
 
-//New states for treaties
-const [treatySearchText, setTreatySearchText] = useState('');
-const [treatyInfo, setTreatyInfo] = useState(null);
-const [treatyError, setTreatyError] = useState(null);
+  // State hooks for treaty data
+  const [treatyName, setTreatyName] = useState('');
+  const [treatyId, setTreatyId] = useState(null);
+  const [treatyTitle, setTreatyTitle] = useState(null);
+  const [treatyError, setTreatyError] = useState(null);
 
-//traties
-const fetchTreatyData = async (searchText) => {
-  try {
-    const response = await fetch(`https://treaties-api.parliament.uk/api/Treaty?SearchText=${encodeURIComponent(searchText)}`);
-    const data = await response.json();
-
-    if (data && data.length > 0) {
-      // Assuming the response structure contains an array of treaties
-      setTreatyInfo(data);
-      setTreatyError(null);
-    } else {
-      setTreatyError('No treaty information found for this search.');
-      setTreatyInfo(null);
-    }
-  } catch (error) {
-    console.error(error);
-    setTreatyError('Failed to fetch treaty information.');
-    setTreatyInfo(null);
-  }
-};
-
-const handleTreatySearchSubmit = (event) => {
-  event.preventDefault();
-  fetchTreatyData(treatySearchText);
-};
-
-
-const handleTreatySearchTextChange = (event) => {
-  setTreatySearchText(event.target.value);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function to perform an HTTP GET request from the Parliament API
-const fetchPartyData = async (name) => {
-
-
-try {
-  //Making an HTTP request to the Parliament api
-  const response = await fetch(`https://members-api.parliament.uk/api/Members/Search?Name=${encodeURIComponent(name)}&skip=0&take=20`);
+  // Function to fetch treaty data from an API
+  const fetchTreatyData = async (treaty) => {
+    try {
+      const response = await fetch(`https://treaties-api.parliament.uk/api/Treaty?SearchText=${encodeURIComponent(treaty)}`);
+      const data = await response.json();
   
-  //response is parsed here
-  const data = await response.json();
+      if (data.items && data.items.length > 0) {
+        // Assuming we are interested in the first treaty found
+        const treatyData = data.items[0].value;
+        setTreatyId(treatyData.id);
+        setTreatyTitle(treatyData.name);
+        // ... set other state variables as needed
+      } else {
+        setTreatyError('Treaty information not found.');
+      }
+    } catch (e) {
+      setTreatyError('Failed to fetch treaty information.');
+      console.error(e);
+    }
+  };
+  
 
-  // Check if the response contains items
-  if (data.items && data.items.length > 0) 
-  {
-    // parsing the data about the party member
-    const member = data.items[0].value;
-    const fullName = member.nameFullTitle; 
-    const memberGender = member.gender;
-    const party = member.latestParty.name;
-    const recentMembership = member.latestHouseMembership.membershipFrom;
-    const startTime = member.latestHouseMembership.membershipStartDate;
-    const endTime = member.latestHouseMembership.membershipEndDate;
-    const statusOfMembership = member.latestHouseMembership.membershipStatus.statusDescription;
+  // Function to fetch party member data from an API
+  const fetchPartyData = async (name) => {
+    try {
+      const response = await fetch(`https://members-api.parliament.uk/api/Members/Search?Name=${encodeURIComponent(name)}&skip=0&take=20`);
+      const data = await response.json();
 
-    //setting the states to the parsed values from the response body
-    setMembershipStatus(statusOfMembership);
-    setMembershipEnd(endTime);
-    setStartDate(startTime);
-    setLatestMembership(recentMembership);
-    setGender(memberGender);
-    setPartyInfo(party);
-    setFullMemberName(fullName);
-  } 
-  // if the response body does not contain any items at all, display this error message 
-  else 
-  {
-  setError('Party information not found for this member.');
-  }
+      if (data.items && data.items.length > 0) {
+        const member = data.items[0].value;
+        setFullMemberName(member.nameFullTitle);
+        setGender(member.gender);
+        setPartyInfo(member.latestParty.name);
+        setLatestMembership(member.latestHouseMembership.membershipFrom);
+        setStartDate(member.latestHouseMembership.membershipStartDate);
+        setMembershipEnd(member.latestHouseMembership.membershipEndDate);
+        setMembershipStatus(member.latestHouseMembership.membershipStatus.statusDescription);
+      } else {
+        setError('Party information not found for this member.');
+      }
+    } catch (e) {
+      setError('Failed to fetch party information.');
+      console.error(e);
+    }
+  };
 
-} 
-// catch any potential errors and display it
-catch (e) 
-{
-  setError('Party information not found for this member.');
-  console.error(e);
-}
+  // Function to handle party member form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    resetMemberInfo();
+    fetchPartyData(memberName);
+  };
 
+  // Function to handle treaty form submission
+  const handleSubmitTreaty = (event) => {
+    event.preventDefault();
+    resetTreatyInfo();
+    fetchTreatyData(treatyName);
+  };
+
+  // Function to reset party member info states
+  const resetMemberInfo = () => {
+    setFullMemberName(null);
+    setPartyInfo(null);
+    setGender(null);
+    setLatestMembership(null);
+    setStartDate(null);
+    setMembershipEnd(null);
+    setMembershipStatus(null);
+    setError(null);
+  };
+
+  // Function to reset treaty info states
+  const resetTreatyInfo = () => {
+    setTreatyId(null);
+    setTreatyTitle(null);
+    setTreatyError(null);
+  };
+
+  // Function to update memberName state when user input changes
+  const handleMemberNameChange = (event) => {
+    setMemberName(event.target.value);
+  };
+
+  // Function to update treatyName state when user input changes
+  const handleTreatyNameChange = (event) => {
+    setTreatyName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Party Information</h2>
+      <h3>Parliament Name Examples: Rishi Sunak, Keir Starmer, Suella Braverman, Grant Shapps etc</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Input Parliament Member's Name:
+          <input
+            type="text"
+            value={memberName}
+            onChange={handleMemberNameChange}
+          />
+        </label>
+        <button type="submit">Display Info</button>
+      </form>
+
+      {error && <p>Error: {error}</p>}
+      {fullMemberName && <p>Name: {fullMemberName}</p>}
+      {gender && <p>Gender: {gender}</p>}
+      {partyInfo && <p>Party: {partyInfo}</p>}
+      {latestMembership && <p>Latest Membership: {latestMembership}</p>}
+      {startDate && <p>Start Date: {startDate}</p>}
+      {membershipEnd && <p>Membership End: {membershipEnd}</p>}
+      {membershipStatus && <p>Membership Status: {membershipStatus}</p>}
+
+      <h2>Treaty Information</h2>
+      <h3>Treaty Name Examples: Treaty of Rome, Treaty of Lisbon etc</h3>
+      <form onSubmit={handleSubmitTreaty}>
+        <label>
+          Input Treaty Name:
+          <input
+            type="text"
+            value={treatyName}
+            onChange={handleTreatyNameChange}
+          />
+        </label>
+        <button type="submit">Display Info</button>
+      </form>
+
+      {treatyError && <p>Error: {treatyError}</p>}
+      {treatyId && <p>Treaty ID: {treatyId}</p>}
+      {treatyTitle && <p>Treaty Title: {treatyTitle}</p>}
+    </div>
+  );
 };
 
-// function for when the user submits the form
-const handleSubmit = (event) => {
-// stops the page from reloading when the form is submitted
-event.preventDefault();
-
-//Reset the previous members data to be null to allow for new data or error message to be displayed
-
-setFullMemberName(null);
-setPartyInfo(null);
-setGender(null);
-setLatestMembership(null);
-setStartDate(null);
-setMembershipEnd(null);
-setMembershipStatus(null);
-
-//resets the value of the error
-setError(null);
-//performs a GET request using the inputted name by the user
-fetchPartyData(memberName);
-
-
-};
-// used to update the member name when the user inputs a different name
-const handleMemberNameChange = (event) => {
-setMemberName(event.target.value);
-};
-
-
-return (
-  <div>
-    {/* Parliament Member Information Section */}
-    <h2>Party Information</h2>
-    <h3>Parliament Name Examples: Rishi Sunak, Keir Starmer, Suella Braverman, Grant Shapps etc.</h3>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Input Parliament Member's Name:
-        <input
-          type="text"
-          value={memberName}
-          onChange={handleMemberNameChange}
-        />
-      </label>
-      <button type="submit">Display Info</button>
-    </form>
-
-    {/* Display errors or member information if available */}
-    {error && <p>Error: {error}</p>}
-    {/* ... other member details displayed here ... */}
-
-    {/* New Treaty Information Section */}
-    <h2>Treaty Information</h2>
-    <form onSubmit={handleTreatySearchSubmit}>
-      <label>
-        Search for a Treaty:
-        <input
-          type="text"
-          value={treatySearchText}
-          onChange={handleTreatySearchTextChange}
-        />
-      </label>
-      <button type="submit">Search Treaty</button>
-    </form>
-
-    {/* Display errors or treaty information if available */}
-    {treatyError && <p>Error: {treatyError}</p>}
-    {treatyInfo && (
-      <div>
-        {treatyInfo.map((treaty, index) => (
-          <div key={index}>
-            <p>Title: {treaty.title}</p>
-          </div>
-        ))}
-      </div>
-    )}
-  </div>
-);
-
-
-        }
 export default Parliament;
