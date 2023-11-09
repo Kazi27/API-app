8e97a516639ee6e03faba6a11a9889e91968424d
Rahatk1 <rahatkhandokar5@gmail.com>
2023-11-08 21:24:05 -0500

    wokring code

--- src/Parliament.js
+++ src/Parliament.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Parliament = () => {
-  // State hooks for party member data
+  // State hooks for member data
   const [memberName, setMemberName] = useState('');
   const [fullMemberName, setFullMemberName] = useState(null);
   const [partyInfo, setPartyInfo] = useState(null);
@@ -12,37 +12,13 @@ const Parliament = () => {
   const [membershipStatus, setMembershipStatus] = useState(null);
   const [error, setError] = useState(null);
 
-  // State hooks for treaty data
-  const [treatyName, setTreatyName] = useState('');
-  const [treatyId, setTreatyId] = useState(null);
-  const [treatyTitle, setTreatyTitle] = useState(null);
-  const [treatyError, setTreatyError] = useState(null);
+  // State hooks for registered interests data
+  const [searchTerm, setSearchTerm] = useState('');
+  const [interests, setInterests] = useState([]);
+  const [interestsError, setInterestsError] = useState(null);
 
-  // Function to fetch treaty data from an API
-  const fetchTreatyData = async (treaty) => {
-    try {
-      const response = await fetch(`https://treaties-api.parliament.uk/api/Treaty?SearchText=${encodeURIComponent(treaty)}`);
-      const data = await response.json();
-      console.log(data); // Log the response for debugging
-
-      if (data.items && data.items.length > 0) {
-        // Assuming we are interested in the first treaty found
-        const treatyData = data.items[0].value;
-        setTreatyId(treatyData.id);
-        setTreatyTitle(treatyData.name);
-        // ... set other state variables as needed
-      } else {
-        setTreatyError('Treaty information not found.');
-      }
-    } catch (e) {
-      setTreatyError('Failed to fetch treaty information.');
-      console.error(e);
-    }
-  };
-  
-
-  // Function to fetch party member data from an API
-  const fetchPartyData = async (name) => {
+  // Function to fetch member data from an API
+  const fetchMemberData = async (name) => {
     try {
       const response = await fetch(`https://members-api.parliament.uk/api/Members/Search?Name=${encodeURIComponent(name)}&skip=0&take=20`);
       const data = await response.json();
@@ -65,21 +41,38 @@ const Parliament = () => {
     }
   };
 
-  // Function to handle party member form submission
-  const handleSubmit = (event) => {
+  // Function to fetch registered interests data from an API
+  const fetchInterestsData = async (term) => {
+    try {
+      const response = await fetch(`https://members-api.parliament.uk/api/LordsInterests/Register?searchTerm=${encodeURIComponent(term)}`);
+      const data = await response.json();
+
+      if (data.items) {
+        setInterests(data.items);
+      } else {
+        setInterestsError('No interests found for the specified search term.');
+      }
+    } catch (e) {
+      setInterestsError('Failed to fetch interests information.');
+      console.error(e);
+    }
+  };
+
+  // Function to handle member form submission
+  const handleMemberSubmit = (event) => {
     event.preventDefault();
     resetMemberInfo();
-    fetchPartyData(memberName);
+    fetchMemberData(memberName);
   };
 
-  // Function to handle treaty form submission
-  const handleSubmitTreaty = (event) => {
+  // Function to handle registered interests form submission
+  const handleInterestsSubmit = (event) => {
     event.preventDefault();
-    resetTreatyInfo();
-    fetchTreatyData(treatyName);
+    resetInterestsInfo();
+    fetchInterestsData(searchTerm);
   };
 
-  // Function to reset party member info states
+  // Function to reset member info states
   const resetMemberInfo = () => {
     setFullMemberName(null);
     setPartyInfo(null);
@@ -91,11 +84,10 @@ const Parliament = () => {
     setError(null);
   };
 
-  // Function to reset treaty info states
-  const resetTreatyInfo = () => {
-    setTreatyId(null);
-    setTreatyTitle(null);
-    setTreatyError(null);
+  // Function to reset registered interests states
+  const resetInterestsInfo = () => {
+    setInterests([]);
+    setInterestsError(null);
   };
 
   // Function to update memberName state when user input changes
@@ -103,16 +95,16 @@ const Parliament = () => {
     setMemberName(event.target.value);
   };
 
-  // Function to update treatyName state when user input changes
-  const handleTreatyNameChange = (event) => {
-    setTreatyName(event.target.value);
+  // Function to update searchTerm state when user input changes
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
   };
 
   return (
     <div>
       <h2>Party Information</h2>
-      <h3>Parliament Name Examples: Rishi Sunak, Keir Starmer, Suella Braverman, Grant Shapps etc</h3>
-      <form onSubmit={handleSubmit}>
+      <h3>Parliament Member Name Examples: Rishi Sunak, Keir Starmer, Suella Braverman, Grant Shapps, etc</h3>
+      <form onSubmit={handleMemberSubmit}>
         <label>
           Input Parliament Member's Name:
           <input
@@ -133,25 +125,33 @@ const Parliament = () => {
       {membershipEnd && <p>Membership End: {membershipEnd}</p>}
       {membershipStatus && <p>Membership Status: {membershipStatus}</p>}
 
-      <h2>Treaty Information</h2>
-      <h3>Treaty Name Examples: Treaty of Rome, Treaty of Lisbon etc</h3>
-      <h3>Union, paris, Brussels</h3>
-
-      <form onSubmit={handleSubmitTreaty}>
+      <h2>Registered Interests</h2>
+      <h3>Search Term Examples: Business, Environment, Health, etc</h3>
+      <form onSubmit={handleInterestsSubmit}>
         <label>
-          Input Treaty Name:
+          Input Search Term:
           <input
             type="text"
-            value={treatyName}
-            onChange={handleTreatyNameChange}
+            value={searchTerm}
+            onChange={handleSearchTermChange}
           />
         </label>
-        <button type="submit">Display Info</button>
+        <button type="submit">Display Interests</button>
       </form>
 
-      {treatyError && <p>Error: {treatyError}</p>}
-      {treatyId && <p>Treaty ID: {treatyId}</p>}
-      {treatyTitle && <p>Treaty Title: {treatyTitle}</p>}
+      {interestsError && <p>Error: {interestsError}</p>}
+
+{interests.length > 0 && (
+  <div>
+    <p>Registered Interests:</p>
+    <ul>
+      {interests.map((interest, index) => (
+        <li key={index}>{interest.value.member.nameFullTitle}</li>
+      ))}
+    </ul>
+  </div>
+)}
+
     </div>
   );
 };
