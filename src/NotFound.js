9756f0ecf20f402e8b6afd30afc2688fa4b9637e
Kazi Anwar <kazisameen2702@gmail.com>
2023-11-16 18:33:09 -0500

    added proper links & minor css

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 const NotFound = () => { //new component
   return (
     <div>
-      <h2 className="parliament-heading">PAGE DOES NOT EXIST</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
+      <h2 className="parliament-heading">No page found</h2>
+      <h2>Sorry, the page you are looking for does not exist.</h2>
     </div> 
   );
 };
